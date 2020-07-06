d37fcc6431a849578ff8e24998398c08cfd826aa
KEERTHINAIK R <keerthinaik.harika@gmail.com>
2020-07-06 11:11:49 +0530

    created a basic react component Person with age and name attributes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import Person from './Person/Person'
 
 class App extends Component {
-  render() {
-    // return (
-    //   <div className="App">
-    //     <h1>Hi, I'm a React app</h1>
-    //   </div>
-    // );
-    return React.createElement('div', {className:'App'}, React.createElement('h1', null, 'HI I\'m React app'));
-  }
+    render() {
+        return (
+            <div className="App">
+                <h1>Hi, I'm a React app</h1>
+                <p>This is really working!</p>
+                <Person name="Max" age="28"/>
+                <Person name="Manu" age="29">My Hobbies: Racing</Person>
+                <Person name="Stephanie" age="26"/>
+            </div>
+        );
+        // return React.createElement('div', {className:'App'}, React.createElement('h1', null, 'HI I\'m React app'));
+    }
 }
 
 export default App;
