e3779c9887c8bc96ec01a942b412e462cc23bfec
KEERTHINAIK R <keerthinaik.harika@gmail.com>
2020-07-07 14:16:11 +0530

    added error handling using error boundary and modularized css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
-import './App.css';
+import classes from './App.css';
 import Person from './Person/Person'
+import ErrorBoundary from "./ErrorBoundary/ErrorBoundary";
 
 class App extends Component {
     state = {
@@ -12,7 +13,6 @@ class App extends Component {
         otherState: 'Some other value',
         showPersons: false
     };
-
 
 
     nameChangedHandler = (event, id) => {
@@ -55,47 +55,44 @@ class App extends Component {
 
     render() {
 
-        const style = {
-            backgroundColor: 'green'
-        };
-
         let persons = null;
-
-        style.backgroundColor = 'green';
+        let btnClass = '';
 
         if (this.state.showPersons) {
-            style.backgroundColor = 'red';
             persons = (
                 <div>
                     {
                         this.state.persons.map((person, index) => {
-                            return <Person
-                                click={() => this.deletePersonHandler(index)}
-                                changed={(event) => this.nameChangedHandler(event, person.id)}
-                                name={person.name}
-                                age={person.age}
-                                key={person.id}
-                            />
+                            return <ErrorBoundary key={person.id}>
+                                <Person
+                                    click={() => this.deletePersonHandler(index)}
+                                    changed={(event) => this.nameChangedHandler(event, person.id)}
+                                    name={person.name}
+                                    age={person.age}
+                                />
+                            </ErrorBoundary>
                         })
                     }
                 </div>
             );
+
+            btnClass = classes.Red
         }
 
-        let classes = [];
+        let assignedClasses = [];
         if (this.state.persons.length <= 2) {
-            classes.push('red');
+            assignedClasses.push(classes.red);
         }
         if (this.state.persons.length <= 1) {
-            classes.push('bold');
+            assignedClasses.push(classes.bold);
         }
 
         return (
-            <div className="App">
+            <div className={classes.App}>
                 <h1>Hi, I'm a React app</h1>
-                <p className={classes.join(' ')}>This is really working!</p>
+                <p className={assignedClasses.join(' ')}>This is really working!</p>
                 <button
-                    style={style}
+                    className={btnClass}
                     onClick={() => this.togglePersonsHandler('Maxmilian!!')}>Toggle Names
                 </button>
                 {persons}
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import './Person.css'
+import classes from './Person.css'
 
 const person = (props) => {
 
-    const style = {
-        '@media (min-width: 500px)': {
-            width: '450px'
-        }
-    };
+    const rnd = Math.random();
+
+    if ( rnd > 0.7 ) {
+        throw new Error('Something went wrong!!');
+    }
 
     return (
-        <div className="Person" style={style}>
+        <div className={classes.Person}>
             <p onClick={props.click}>I'm {props.name} and {props.age} years old.</p>
             <input type="text" onChange={props.changed} value={props.name}/>
         </div>
